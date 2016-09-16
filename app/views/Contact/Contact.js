dbffa7fc17db0cfa67cdc574f9494826086e86db
Alexander Esp <alexjesp@gmail.com>
2016-09-16 10:09:33 +0100

    Tidy up contact page

--- app/views/Contact/Contact.js
+++ app/views/Contact/Contact.js
@@ -8,6 +8,9 @@ const Contact = () => {
         <input className='Contact-input' type='text' placeholder='Name' />
         <input className='Contact-input' type='email' placeholder='Email' />
         <textarea className='Contact-input Contact-textbox' placeholder='Message' />
+        <div className='Contact-footer'>
+          <input className='Contact-submit Contact-input' type='submit' value='Submit' />
+        </div>
       </form>
     </div>
   )
