15e22e79f5268749abcd7e9dc5fd7d451995a5f5
Alexander Esp <alexjesp@gmail.com>
2016-11-19 08:33:00 +0000

    Add MOTB still

--- app/views/Media/Media.js
+++ app/views/Media/Media.js
@@ -8,6 +8,7 @@ class Media extends React.Component {
         <img className='Media-image' src='/images/theyachtclub-fall-promo.jpg' />
         <div className='Media-credit'>Photo taken by <a className='Media-creditLink' href='https://www.facebook.com/CatherineBridgmanPhotographer' target='_blank'>Catherine Bridgman</a></div>
         <img className='Media-image' src='/images/gmisadventure-still.jpg' />
+        <img className='Media-image' src='/images/motb-video.jpg' />
         <img className='Media-image' src='/images/c2c.jpg' />
         <div className='Media-videoWrapper'>
           <iframe width='560' height='315' src='https://www.youtube.com/embed/p2E2qEtytTY?VQ=HD720' frameBorder='0' allowFullScreen />
