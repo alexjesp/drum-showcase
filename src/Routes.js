2c0a65db0ca667b043adeb686802cacf71e5a652
Alexander Esp <alexjesp@gmail.com>
2019-11-03 21:47:12 +0000

    Simplify server and deployment

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,11 @@ import { Route } from 'react-router'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { App } from './App'
 
-export default function Routes () {
-  return (
-    <BrowserRouter>
+export const Routes = () => (
+  <BrowserRouter>
     <Switch>
       <Route path='/:pageType' component={App} />
       <Route path="/" component={App} />
     </Switch>
-    </BrowserRouter>
-  )
-}
+  </BrowserRouter>
+)
