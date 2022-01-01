60ba3c9721f67326a97ad2450bc69a660f021935
Alexander Esp <alexjesp@gmail.com>
2022-01-01 14:19:45 +0000

    build using sveltejs

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<img
+	src="/images/IMG_2921.jpg"
+	alt="Alexander Esp playing piano in a denim jacket at Bookhouse Studios"
+/>
+<div class="img-credit">Photo by Treasure Digital.</div>
+
+<style>
+	img {
+		width: 100%;
+		height: auto;
+	}
+
+	.img-credit {
+		font-size: 12px;
+		color: #444;
+		text-align: right;
+		margin-top: 1em;
+		margin-bottom: 2em;
+	}
+</style>
