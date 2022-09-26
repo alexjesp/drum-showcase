8b5bb1c5166383b53e315ecee22ef51230344dfe
Alexander Esp <contact@alexesp.co.uk>
2022-09-26 08:47:13 +0100

    add young astronaut and little victories singles

--- src/data/releaseItems.ts
+++ src/data/releaseItems.ts
@@ -7,77 +7,92 @@ export type Release = {
   description: string
 }
 
-export const releases: Release[] = [
-  {
-    imgSrc: '../../images/hope-single-cover.jpg',
-    href: 'https://open.spotify.com/album/4PLisKJNR5TN8A4nvwdQYX?si=GfYngFn8QhCYVImw-654Kw',
-    title: 'Hope',
-    type: 'Single',
-    artist: "Young Astronaut",
-    description: 'Drums'
-  },
-  {
-    imgSrc: '../../images/lcpc-everything-is-fine-ep.jpg',
-    href: 'https://leecarvallosputtingchallenge.bandcamp.com',
-    title: 'Everything Is Fine',
-    type: 'EP',
-    artist: "Lee Carvallo's Putting Challenge",
-    description: 'Drums'
-  },
-  {
-    imgSrc: '../../images/theyachtclub-lp1-front.jpg',
-    href: 'https://open.spotify.com/album/01IS5APLJsk4Q7ungYwp9O?si=yxNGJzlpTHaydT8_NIi7mA',
-    title: 'The Last Words That You Said To Me Have Kept Me Here And Safe',
-    type: 'LP',
-    artist: 'The Yacht Club',
-    description: 'Drums, Piano, Rhodes, Organ, Percussion and Backing Vocals'
-  },
-  {
-    imgSrc: '../../images/theyachtclub-fall.jpg',
-    href: 'https://theyachtclub.bandcamp.com/album/fall',
-    title: 'Fall',
-    type: 'EP',
-    artist: 'The Yacht Club',
-    description: 'Drums, Piano, Rhodes, Organ, Percussion'
-  },
-  {
-    imgSrc: '../../images/raad-indignities.jpg',
-    href: 'http://raadp.bandcamp.com/releases',
-    title: 'Indignities',
-    type: 'EP',
-    artist: 'RAAD',
-    description: 'Drums, Percussion'
-  },
-  {
-    imgSrc: '../../images/theyachtclub-self.jpg',
-    href: 'https://theyachtclub.bandcamp.com/album/the-yacht-club',
-    title: 'The Yacht Club',
-    type: 'EP',
-    artist: 'The Yacht Club',
-    description: 'Drums, Percussion, Keyboards'
-  },
-  {
-    imgSrc: '../../images/theyachtclub-a.jpg',
-    href: 'https://theyachtclub.bandcamp.com/album/a',
-    title: 'A',
-    type: 'EP',
-    artist: 'The Yacht Club',
-    description: 'Drums, Percussion'
-  },
-  {
-    imgSrc: '../../images/outliers.jpg',
-    href: 'https://soundcloud.com/thomashillrecording/rock-outliers-home',
-    title: 'Home',
-    type: 'Single',
-    artist: 'Outliers',
-    description: 'Drums'
-  },
-  {
-    imgSrc: '../../images/daphnetae.jpg',
-    href: 'https://toarmsetcofficial.bandcamp.com/album/the-ghost-of-daphne-workshy',
-    title: 'The Ghost of Daphne',
-    type: 'Single',
-    artist: 'To Arms Etc',
-    description: 'Drums'
-  }
+export const releases: Release[] = [{
+  imgSrc: '../../images/lv-not-giving-up.jpg',
+  href: 'https://ourlittlevictories.bandcamp.com/track/not-giving-up',
+  title: 'Not Giving Up',
+  type: 'Single',
+  artist: "Little Victories",
+  description: 'Drums'
+},
+{
+  imgSrc: '../../images/hellions-single-cover.jpg',
+  href: 'https://open.spotify.com/album/1U79XUlANDpMlnZQuUTs23?si=wfaObvcZQlaNjfAh_2n9mw',
+  title: 'Hellions',
+  type: 'Single',
+  artist: "Young Astronaut",
+  description: 'Drums'
+},
+{
+  imgSrc: '../../images/hope-single-cover.jpg',
+  href: 'https://open.spotify.com/album/4PLisKJNR5TN8A4nvwdQYX?si=GfYngFn8QhCYVImw-654Kw',
+  title: 'Hope',
+  type: 'Single',
+  artist: "Young Astronaut",
+  description: 'Drums'
+},
+{
+  imgSrc: '../../images/lcpc-everything-is-fine-ep.jpg',
+  href: 'https://leecarvallosputtingchallenge.bandcamp.com',
+  title: 'Everything Is Fine',
+  type: 'EP',
+  artist: "Lee Carvallo's Putting Challenge",
+  description: 'Drums'
+},
+{
+  imgSrc: '../../images/theyachtclub-lp1-front.jpg',
+  href: 'https://open.spotify.com/album/01IS5APLJsk4Q7ungYwp9O?si=yxNGJzlpTHaydT8_NIi7mA',
+  title: 'The Last Words That You Said To Me Have Kept Me Here And Safe',
+  type: 'LP',
+  artist: 'The Yacht Club',
+  description: 'Drums, Piano, Rhodes, Organ, Percussion and Backing Vocals'
+},
+{
+  imgSrc: '../../images/theyachtclub-fall.jpg',
+  href: 'https://theyachtclub.bandcamp.com/album/fall',
+  title: 'Fall',
+  type: 'EP',
+  artist: 'The Yacht Club',
+  description: 'Drums, Piano, Rhodes, Organ, Percussion'
+},
+{
+  imgSrc: '../../images/raad-indignities.jpg',
+  href: 'http://raadp.bandcamp.com/releases',
+  title: 'Indignities',
+  type: 'EP',
+  artist: 'RAAD',
+  description: 'Drums, Percussion'
+},
+{
+  imgSrc: '../../images/theyachtclub-self.jpg',
+  href: 'https://theyachtclub.bandcamp.com/album/the-yacht-club',
+  title: 'The Yacht Club',
+  type: 'EP',
+  artist: 'The Yacht Club',
+  description: 'Drums, Percussion, Keyboards'
+},
+{
+  imgSrc: '../../images/theyachtclub-a.jpg',
+  href: 'https://theyachtclub.bandcamp.com/album/a',
+  title: 'A',
+  type: 'EP',
+  artist: 'The Yacht Club',
+  description: 'Drums, Percussion'
+},
+{
+  imgSrc: '../../images/outliers.jpg',
+  href: 'https://soundcloud.com/thomashillrecording/rock-outliers-home',
+  title: 'Home',
+  type: 'Single',
+  artist: 'Outliers',
+  description: 'Drums'
+},
+{
+  imgSrc: '../../images/daphnetae.jpg',
+  href: 'https://toarmsetcofficial.bandcamp.com/album/the-ghost-of-daphne-workshy',
+  title: 'The Ghost of Daphne',
+  type: 'Single',
+  artist: 'To Arms Etc',
+  description: 'Drums'
+}
 ]
